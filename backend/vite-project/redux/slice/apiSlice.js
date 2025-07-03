4fbcd7945e0846ef47d2fb1031bda6033543e4a9
hxn9999 <hh8391781@gmail.com>
2025-07-03 09:52:46 +0600

    new build

--- backend/vite-project/redux/slice/apiSlice.js
+++ backend/vite-project/redux/slice/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi ,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath:'api',
     baseQuery:fetchBaseQuery({
-        baseUrl:"http://localhost:3000",
+        baseUrl:"http://46.202.166.219:3003/",
         
     }),
     endpoints:(builder)=>({
